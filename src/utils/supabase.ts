1fb9ba670bac7eec4cf04709ff380f1fba533177
Rain Tolentino <rain.tolentino.uni@gmail.com>
2025-12-12 18:04:07 +1100

    remove api key

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,8 +1,8 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://fwqjozrzejpbzjvotlnd.supabase.co'
-const supabasePublishableKey = 'sb_publishable_jqaIOGWGXNG65wYwpvBuig__uVqu4wI'
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!
+const supabasePublishableKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!
 
 export const supabase = createClient(supabaseUrl, supabasePublishableKey, {
   auth: {
