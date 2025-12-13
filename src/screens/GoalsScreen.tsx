753f07e20c9e2ff66083b799b6bacf308f1b1a5e
Aman Jifry <z5592111@ed.unsw.edu.au>
2025-12-13 20:06:43 +0400

    changed rendering of habit/goal creation boxes.
    habits now render above goals mamking the program more intuitive.
    also, they are no longer rendered as flex boxes and instead have a 10% gap between them for a more centered look.

--- src/screens/GoalsScreen.tsx
+++ src/screens/GoalsScreen.tsx
@@ -5,15 +5,14 @@ import { CreateHabit } from '../components/goals/CreateHabit';
 
 export const GoalsScreen = () => {
   return (
-    <SafeAreaView className="flex-1 bg-[#030712]">
-      <View className="flex-1 items-center justify-center">
-        <Text className="text-2xl font-bold text-white">Goals</Text>
-        <CreateGoal />
-      </View>
-      {/* TODO: i left this here to test, feel free to remove */}
-      <View className="flex-1 items-center justify-center">
+    <SafeAreaView className="flex-1 gap-10 justify-center bg-[#030712]">
+      <View className=" items-center justify-center">
         <Text className="text-2xl font-bold text-white">Habits</Text>
         <CreateHabit />
+      </View>
+      <View className=" items-center justify-center">
+        <Text className="text-2xl font-bold text-white">Goals</Text>
+        <CreateGoal />
       </View>
     </SafeAreaView>
   );
