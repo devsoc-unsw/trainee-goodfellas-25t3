425146294992d1aa43db4534c6b0756db3e19073
Rain Tolentino <rain.tolentino.uni@gmail.com>
2025-12-14 23:26:00 +1100

    add backend logic for updating a habit
    still need to fix the rls policy for it to work though

--- src/services/habitServices.ts
+++ src/services/habitServices.ts
@@ -1,6 +1,20 @@
 import { Session } from "@supabase/supabase-js";
 import { supabase } from "../utils/supabase";
 
+export async function getHabit(session:Session, id:number) {
+  const { data, error } = await supabase
+    .from('habits')
+    .select('*')
+    .eq('user_id', session.user.id)
+    .eq('id', id)
+    .single();
+
+  if (error) {
+    return { error: error.message }
+  }
+  return { habit: data };
+}
+
 export async function fetchHabits(session: Session|null) {
   if (!session?.user) {
       return { error: 'Must be logged in to fetch habits.' };
@@ -54,6 +68,43 @@ export async function deleteHabit(session:Session|null, id:number) {
     .from('habits')
     .delete().in('id', [id])
     .eq('user_id', session.user.id);
+  if (error) {
+    return { error: error.message };
+  }
+}
+
+// TODO: this works, just need to fix RLS policy for updating
+export async function updateHabit(
+  session:Session|null, id: number, name?: string, description?: string) {
+  if (!session?.user) {
+    return { error: 'Must be logged in to edit a habit.' };
+  }
+
+  const {error: err, habit} = await getHabit(session, id);
+  if (err) {
+    return { error: err };
+  }
+  if (!habit) {
+    return { error: "Couldn't retrieve the original data." };
+  }
+
+  const updated = {
+    id: habit.id,
+    created_at: habit.created_at,
+    name: name || habit.name,
+    description: description || habit.description || null,
+    total_hours: habit.total_hours,
+    user_id: habit.user_id
+  };
+
+  console.log(`new stuff is ${name} and ${description} we update ${id}`);
+  console.log(`old stuff is ${habit.name} and ${habit.description}`);
+  const { data, error } = await supabase
+    .from('habits')
+    .update(updated)
+    .eq('user_id', session.user.id)
+    .eq('id', id);
+
   if (error) {
     return { error: error.message };
   }
