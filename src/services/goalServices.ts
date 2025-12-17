03bf37f6e1c92f07c5f09ae75fc288d934c2c7fb
Rain Tolentino <rain.tolentino.uni@gmail.com>
2025-12-18 03:19:32 +1100

    add goal update function

--- src/services/goalServices.ts
+++ src/services/goalServices.ts
@@ -53,3 +53,36 @@ export async function deleteGoal(id: number) {
     return { error: error.message }
   }
 }
+
+export async function updateGoal(
+  id: number, name?:string, description?:string, habitId?: number, completed?: number)
+{
+  const { goal, error: err } = await getGoal(id);
+  
+  if (err) {
+    return  { error: err };
+  }
+  if (!goal) {
+    return { error: "Couldn't retrieve the original data."};
+  }
+
+  const updated = {
+    id: goal.id,
+    created_at: goal.created_at,
+    name: name || goal.name,
+    description: description || goal.description || null,
+    hours_required: goal.hours_required,
+    hours_completed: completed || goal.hours_completed,
+    habit_id: habitId || goal.habit_id
+  };
+
+  // TODO: should i be moving this logic to a postgresql func since it doesn't check for ownership?
+  const { data, error } = await supabase
+    .from('goals')
+    .update(updated)
+    .eq('id', id);
+  
+  if (error) {
+    return { error: error.message };
+  }
+}
