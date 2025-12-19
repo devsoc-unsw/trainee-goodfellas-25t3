0b13a5a451a2c459c216cfd1295bed5a0e750bbc
Jacob Cubillo <jacobcubillo@gmail.com>
2025-12-19 21:35:30 +1100

    edit habit form

--- src/screens/EditHabitScreen.tsx
+++ src/screens/EditHabitScreen.tsx
@@ -1,7 +1,6 @@
-import { Text, View, ActivityIndicator, TextInput, ScrollView, TouchableOpacity } from 'react-native';
+import { Text, View, ActivityIndicator, TextInput, ScrollView, TouchableOpacity, Button } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Habit } from '../types/habit';
-import { Goal } from "../types/goal";
 import { useEffect, useState, useMemo } from "react";
 import { useSession } from '../contexts/SessionContext';
 import { fetchGoals as getGoals, updateGoal } from '../services/goalServices';
@@ -16,7 +15,44 @@ interface EditHabitScreenProps {
 
 export const EditHabitScreen = ({ route }: EditHabitScreenProps) => {
   const habit = route?.params?.habit;
+  const { session } = useSession()
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [hours, setHours] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
 
+  useEffect(() => {
+    setName(habit?.name || "");
+    setDescription(habit?.description || "");
+  }, [])
+
+  const handleEditHabit = async () => {
+    if (!session?.user) {
+      setError("Must be logged in to edit a goal.");
+      return;
+    }
+
+    setLoading(true);
+    setError(null);
+    
+    // const ret = await createGoal(session, name, description, selectedHabit, parseInt(hours, 10));
+    alert("create an edit HABIT call to the supabase (1) handle name checking as well, returning an error if no name");
+
+    setLoading(false);
+
+    // if (ret?.error) {
+    //   setError(ret?.error);
+    //   console.error(error);
+    // } else {
+    //   setName("");
+    //   setDescription("");
+    //   setHours("");
+    //   setHabit(null);
+    //   onSuccess?.(); // Notify parent of success
+    // }
+  };
+  
   return (
     <SafeAreaView className="flex-1 bg-[#030712]">
       <ScrollView className="flex-1" showsVerticalScrollIndicator={false}>
@@ -30,13 +66,30 @@ export const EditHabitScreen = ({ route }: EditHabitScreenProps) => {
               </View>
             ) : (
               <View>
-                <Text className="text-3xl font-bold text-white text-center mb-2">
+                <Text className="text-3xl font-bold text-white text-center mb-10">
                   Edit "{habit.name}"
                 </Text>
 
-                <View>
-                  
-
+                <View className="flex flex-col gap-4">
+                  <TextInput
+                    className="w-72 h-12 px-4 rounded-lg bg-neutral-900 text-white border border-neutral-700"
+                    placeholder="Habit Name"
+                    placeholderTextColor='#888'
+                    value={name}
+                    onChangeText={setName}
+                  />
+                  <TextInput
+                    className="w-72 h-12 px-4 rounded-lg bg-neutral-900 text-white border border-neutral-700"
+                    placeholder="Description (optional)"
+                    placeholderTextColor='#888'
+                    value={description}
+                    onChangeText={setDescription}
+                  />
+                  <Button
+                    title={loading ? "Creating..." : "Create Habit"}
+                    onPress={handleEditHabit}
+                    disabled={loading}
+                  />
                 </View>
               </View>
             )}
